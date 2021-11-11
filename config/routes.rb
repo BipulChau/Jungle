4b278c7c016f06a8e469abcfd10610e2a84ac055
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-11 17:12:16 -0500

    Created models, migrated models and created constroller for users

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
 
   get '/about', to: 'about#index'
 
+  # Show registration form and create new users
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
